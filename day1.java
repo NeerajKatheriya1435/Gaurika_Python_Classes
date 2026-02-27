ef791deadf6fe95b9a2585b3a58a689b4b34713c
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-27 13:10:51 +0530

    loops and functions

---
+++ day1.java
@@ -0,0 +1,8 @@
+
+// public class day1{
+//     public static void main(String[] args){
+//         for (int i = 1; i < 21; i=i*3) {
+//             System.out.println(i);
+//         }
+//     }
+// }
